a431d8cedae2ab906b6442247f9af72e7282aa37
boris6 <sijanb@outlook.com>
2023-06-04 09:16:14 +0200

    Prepare drawer for filters screen

--- meals/lib/screens/tabs.dart
+++ meals/lib/screens/tabs.dart
@@ -44,6 +44,13 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
+  void _setScreen(String identifier) {
+    if (identifier == 'filters') {
+    } else {
+      Navigator.of(context).pop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget activePage = CategoriesScreen(
@@ -63,7 +70,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
-      drawer: MainDrawer(),
+      drawer: MainDrawer(onSelectScreen: _setScreen),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: (index) {
